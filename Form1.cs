93514ed11284139ec3a8cf9d58c92b4952e2b007
lekimanh452003 <101647674+lekimanh452003@users.noreply.github.com>
2024-04-07 16:17:01 +0700

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BTVN_Bai1_LienKetWebSite
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            txtKQ.Clear();  
+        }
+
+        private void btnOK_Click(object sender, EventArgs e)
+        {
+            if(IstBox.SelectedItems!=null)
+            {
+               string selectedWeb=IstBox.SelectedItem.ToString();
+                txtKQ.Text = "Bạn đã chọn " + selectedWeb;
+            }
+        }
+    }
+}
