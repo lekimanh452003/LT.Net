6aa1d883a660627ccf659ed2296203c2c438fe97
lekimanh452003 <101647674+lekimanh452003@users.noreply.github.com>
2024-04-07 16:57:43 +0700

    Add files via upload

---
+++ ComboboxLienKetWebsite.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BTVN_Bai1_LienKetWebSite
+{
+    public partial class ComboboxLienKetWebsite : Form
+    {
+        public ComboboxLienKetWebsite()
+        {
+            InitializeComponent();
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            txtKQ.ResetText();
+        }
+
+        private void btnOK_Click(object sender, EventArgs e)
+        {
+            if(cbBox.SelectedItem != null)
+            {
+                string slWeb=cbBox.SelectedItem.ToString();
+                txtKQ.Text = "Bạn đã chọn "+slWeb;
+            }
+        }
+    }
+}
